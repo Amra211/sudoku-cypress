2c9f1ba6a94f783125833a75733b752d8966197f
AmraS <amra.sijak98@gmail.com>
2023-05-11 23:16:46 +0200

    Blazemeter module 11
    - learned how tick and clock works

--- cypress/e2e/timer.cy.ts
+++ cypress/e2e/timer.cy.ts
@@ -4,8 +4,16 @@ describe('Timer', () => {
     for(let k = 0; k < 10; k++) {
       cy.contains('.status__time', `00:0${k}`)
     }
-    
-    
-    
   })
-})
+    
+    //confirm that the timer shows "00:30" after 30 seconds
+    //confirm that the timer shows "11:40" after 700 seconds
+    it.only('Shows minutes and seconds since the game started', () => {
+      cy.clock()
+      cy.visit('/')
+      cy.contains('.status__time', '00:00')
+      cy.tick(30_000)
+      //cy.contains('.status__time', '00:30')
+      // ne radi tick
+    })
+  })
